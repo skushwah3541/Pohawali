afd526162f15d492013b295704c2d6ad3c714524
skushwah3541 <s_kushwah@hotmail.com>
2015-09-20 20:56:14 -0400

    updated

--- poha/lib/DataStore/Poha/Schema.pm
+++ poha/lib/DataStore/Poha/Schema.pm
@@ -15,9 +15,9 @@ __PACKAGE__->load_namespaces;
 # Created by DBIx::Class::Schema::Loader v0.07039 @ 2014-03-18 20:46:42
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:lDymkjfEWjCTe/YrOXGS/g
 
-my $database = 'poha';
-my $user = 'service';
-my $password = 'sandkush';
+my $database = 'abc';
+my $user = 'pqr';
+my $password = 'xyz';
 
 __PACKAGE__->connection(
   "dbi:mysql:database=$database;host=localhost",$user,$password
